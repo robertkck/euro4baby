6da734c8bda73f566e73c4a5bf5c50dfd76691a2
robertkck <robert_kck@hotmail.com>
2022-09-11 17:48:10 +0200

    equal karenz

--- app.R
+++ app.R
@@ -19,7 +19,9 @@ order_labels <- tibble(
     value = order_choices |>
       purrr::map(~paste("Elternteil", .x))
   )
-
+format_date <- function(date){
+  lubridate::stamp("1. Mai, 1999", locale = "de")(date)
+}
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -83,7 +85,7 @@ ui <- fluidPage(
                 numericInput(
                   "period1",
                   "Periode 1",
-                  value = 365,
+                  value = 213,
                   min = 0,
                   max = 365
                 )
@@ -93,7 +95,7 @@ ui <- fluidPage(
                 numericInput(
                   "period2",
                   "Periode 2",
-                  value = 61,
+                  value = 213,
                   min = 0,
                   max = 365
                 )
@@ -107,18 +109,29 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
+           # fluidRow(
+           #   column(
+           #     width = 6,
+           #     mod_income_ui("mother")
+           #    ),
+           #   column(
+           #     width = 6,
+           #     mod_income_ui("father")
+           #   ),
+           #   # mod_timeframe_ui("timeframe"),
+           #   # verbatimTextOutput("totalReturnTogether")
+           #  ),
            fluidRow(
              column(
                width = 6,
-               mod_income_ui("mother")
-              ),
+               uiOutput("totals")
+             ),
              column(
                width = 6,
-               mod_income_ui("father")
-             ),
-             mod_timeframe_ui("timeframe"),
-             verbatimTextOutput("totalReturnTogether")
-            ),
+               echarts4rOutput("modelle")
+             )
+           ),
+           h3("Zahlungen"),
            # Exact calendar with https://dreamrs.github.io/toastui/articles/extras/calendar.html
            echarts4r::echarts4rOutput("figPayments")
         )
@@ -175,19 +188,18 @@ server <- function(input, output) {
 
 # Calculate payments ------------------------------------------------------
 
+    r.days <- mod_timeframe_server(
+      "timeframe",
+      order = reactive(order_parsed()),
+      exactDate = reactive(input$exactDate),
+      birthDate = reactive(input$birthDate),
+      period1 = reactive(input$period1),
+      period2 = reactive(input$period2),
+      period3 = reactive(input$period3)
+    )
+
     r.payments <- reactive({
       req(r.incomeMother(), r.incomeFather())
-
-      validate(need(is.numeric(input$period1), "yo"))
-      r.days <- mod_timeframe_server(
-        "timeframe",
-        order = reactive(order_parsed()),
-        exactDate = reactive(input$exactDate),
-        birthDate = reactive(input$birthDate),
-        period1 = reactive(input$period1),
-        period2 = reactive(input$period2),
-        period3 = reactive(input$period3)
-      )
 
       tbl.payments <- r.days() |>
         mutate(
@@ -206,7 +218,50 @@ server <- function(input, output) {
 
 # Calculate totals ---------------------------------------------------------
 
+    output$totals <- renderUI({
 
+      tagList(
+        h3("Voraussetzungen"),
+        p(tags$b("Durchgehende Beschäftigung Mutter seit: "),
+          format_date(input$birthDate - lubridate::weeks(8) - lubridate::days(182))
+        ),
+        p(tags$b("Durchgehende Beschäftigung Vater seit: "),
+          format_date(input$birthDate - lubridate::days(182))
+        ),
+        h3("Wichtige Daten"),
+        p(tags$b("Beginn Mutterschutz:"), format_date(input$birthDate - lubridate::weeks(8))),
+        p(tags$b("Geburt: "), format_date(input$birthDate)),
+        p(tags$b("Ende der Kinderbetreuung: "), format_date(max(r.days()$date))),
+        p(
+          tags$b("Dauer der Kindergeld: "),
+          r.days() |>
+            filter(periodType == "Kindergeld") |>
+            summarise(dauer = max(date) - min(date)) |>
+            pull(dauer),
+          " Tage"
+        )
+      )
+    })
+
+    output$modelle <- renderEcharts4r({
+      tbl.payments <- r.payments() |>
+        group_by(parent, periodType) |>
+        summarise(value = sum(value, na.rm = TRUE)) |>
+        mutate(model = "Einkommensabhängiges Kinderbetreuungsgeld")
+
+      tbl.modelle <- bind_rows(
+        tbl.payments,
+        tbl.payments |>
+          filter(periodType == "Mutterschutz") |>
+          mutate(model = "Kinderbetreuungsgeld Konto")
+      )
+
+      tbl.modelle |>
+        group_by(paste(parent, periodType)) |>
+        e_charts(model) |>
+        e_bar(value, stack = "stack") |>
+        e_tooltip()
+    })
 
 
 # Chart -------------------------------------------------------------------
@@ -227,7 +282,6 @@ server <- function(input, output) {
         group_by(paste(parent, periodType)) |>
         echarts4r::e_charts(month) |>
         echarts4r::e_bar(value, stack = "stack") |>
-        echarts4r::e_title("Zahlungen") |>
         echarts4r::e_tooltip() |>
         e_mark_line(data = list(xAxis = as.Date(input$birthDate)), title = "Geburt")
     })
