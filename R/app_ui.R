c06cb176cb0f50046a259b37708a6ec68f4590d5
Robert Hawlik <robert.hawlik@wikifolio.com>
2022-10-02 20:51:42 +0200

    change title

--- R/app_ui.R
+++ R/app_ui.R
@@ -205,7 +205,7 @@ golem_add_external_resources <- function() {
     favicon(ext = 'png'),
     bundle_resources(
       path = app_sys("app/www"),
-      app_title = "euro4baby"
+      app_title = "Kinderbetreuungsgeld Planer"
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
